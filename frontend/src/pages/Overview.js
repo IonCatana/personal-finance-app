c37d879f70c5fa66084f8971cf306f124f450f04
IonCatana <catana.ion17@yahoo.it>
2024-11-26 15:27:10 +0100

    Aggiungi la gestione dei budget nella pagina Overview; migliora la disposizione dei componenti e integra il nuovo componente SectionHeaderCard

--- frontend/src/pages/Overview.js
+++ frontend/src/pages/Overview.js
@@ -5,9 +5,12 @@ import { useTheme } from "@mui/material/styles";
 import StatCard from "@components/card/StatCard";
 import PotsOverview from "@components/pots/PotsOverview";
 import SectionHeaderContent from "@components/headers/SectionHeaderContent";
+import SectionHeaderCard from "@components/card/SectionHeaderCard";
+import { useMenu } from "@context/MenuContext";
 
 const OverviewContent = () => {
   const theme = useTheme();
+  const { setActiveMenu } = useMenu();
 
   const userToken = localStorage.getItem("token");
   return (
@@ -31,8 +34,41 @@ const OverviewContent = () => {
         <StatCard title="Expenses" value="$1,700.50" />
       </Box>
       {/* PotsOverview */}
-      <Box>
-        <PotsOverview token={userToken} />
+      <Box
+        className="columns"
+        sx={{
+          display: "flex",
+          gap: pxToRem(24),
+          flexDirection: {
+            xs: "column",
+            sm: "column",
+            md: "column",
+            lg: "row",
+          },
+        }}>
+        <Box
+          className="column-left"
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            gap: { xs: pxToRem(16), sm: pxToRem(24), md: pxToRem(24) },
+          }}>
+          <PotsOverview token={userToken} />
+        </Box>
+        <Box
+          className="column-right"
+          sx={{
+            maxWidth: { xs: "100%", sm: "100%", md: pxToRem(428) },
+            width: "100%",
+            border: `1px solid ${theme.palette.grey[900]}`,
+          }}>
+          <SectionHeaderCard
+            title="Budgets"
+            buttonLabel="See Details"
+            onButtonClick={() => setActiveMenu(3)}
+          />
+        </Box>
       </Box>
     </Box>
   );
