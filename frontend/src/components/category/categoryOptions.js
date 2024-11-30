4d322b8f8527a087debdb37e43f6fd5758005e47
IonCatana <catana.ion17@yahoo.it>
2024-11-30 23:46:06 +0100

    Aggiungi le opzioni di categoria per la gestione delle transazioni nel componente categoryOptions

---
+++ frontend/src/components/category/categoryOptions.js
@@ -0,0 +1,13 @@
+export const categoryOptions = [
+  { label: "All Transactions", value: "all-transactions" },
+  { label: "General", value: "general" },
+  { label: "Dining Out", value: "dining-out" },
+  { label: "Groceries", value: "groceries" },
+  { label: "Entertainment", value: "entertainment" },
+  { label: "Transportation", value: "transportation" },
+  { label: "Lifestyle", value: "lifestyle" },
+  { label: "Personal Care", value: "personal-care" },
+  { label: "Education", value: "education" },
+  { label: "Bills", value: "bills" },
+  { label: "Shopping", value: "shopping" },
+];
