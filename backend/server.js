3f0e97deb704f283e3a0ab0c5d4ef6a390ec518d
IonCatana <catana.ion17@yahoo.it>
2024-12-11 15:48:18 +0100

    Aggiungi supporto per servire il frontend in modalità produzione

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,14 @@ app.use("/api/pots", authMiddleware, potRoutes);
 app.use("/api/budgets", authMiddleware, budgetRoutes);
 app.use("/api/transactions", authMiddleware, transactionRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+  );
+}
+
 // Porta di ascolto
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
